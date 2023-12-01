6db4f6ca3aa144a2de1bb56515d35ee501c036f2
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-12-01 15:31:14 +0000

    Add new files and update dependencies

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# yarn-ts-template
+# Advent of Code 2023
--- bin/build.ts
+++ bin/build.ts
@@ -1,36 +1,26 @@
 import { build, BuildOptions } from "esbuild";
-import { statSync } from "node:fs";
-import { resolve } from "node:path";
-import { format } from "./pretty-bytes";
-import { cyan, green } from "console-log-colors";
+import { green } from "console-log-colors";
 
-interface Options extends BuildOptions {
-	outfile: string;
-}
-
-const options: Options = {
-	entryPoints: ["./src/index.ts"],
-	minify: true,
-	bundle: true,
-	outfile: "./dist/index.js",
-	target: "node20",
-	platform: "node",
-	format: "esm",
-	sourcemap: false,
+const options: BuildOptions = {
+  entryPoints: ["./src/**/index.ts"],
+  minify: true,
+  bundle: true,
+  outdir: "./dist",
+  target: "node20",
+  platform: "node",
+  format: "esm",
+  sourcemap: false,
 };
 
 // Log success message
 const logSuccess = () => {
-	const outfile = options.outfile;
-	const distSize = statSync(resolve(outfile)).size;
-	console.log(outfile, "|", cyan(format(distSize)));
-	console.log(green("\u{2714} Finished successfully!"));
+  console.log(green("\u{2714} Finished successfully!"));
 };
 
 // Build and log result
 build(options)
-	.catch((err) => {
-		console.error(err);
-		process.exit(1);
-	})
-	.then(logSuccess);
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .then(logSuccess);
