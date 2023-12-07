a4c810a717d4cfa836722a660d036ecb77b3f401
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-12-07 04:29:55 +0000

    Fix bug in login functionality

---
+++ src/day6/mod.ts
@@ -0,0 +1,33 @@
+// https://adventofcode.com/2023/day/5
+
+const solvePart1 = (): number => {
+  return 0;
+};
+
+const solvePart2 = (): number => {
+  return 0;
+};
+
+const solve = async (file: string): Promise<[number, number]> => {
+  const input = await Deno.readTextFile(
+    new URL(import.meta.resolve(file)),
+  );
+
+  const answerPart1 = solvePart1();
+  const answerPart2 = solvePart2();
+
+  return [answerPart1, answerPart2];
+};
+
+const main = async () => {
+  const [answerPart1, answerPart2] = await solve("./input.txt");
+
+  console.log(`Part 1: ${answerPart1}`);
+  console.log(`Part 2: ${answerPart2}`);
+};
+
+if (import.meta.main) {
+  await main();
+}
+
+export { solve };
